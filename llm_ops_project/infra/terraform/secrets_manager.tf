9803ff0f19d566144d5365b46befc6b2b7fdb095
davestroud <david@davidstroud.me>
2024-12-27 12:08:42 -0500

    update secrets manager tf

--- llm_ops_project/infra/terraform/secrets_manager.tf
+++ llm_ops_project/infra/terraform/secrets_manager.tf
@@ -1,11 +1,13 @@
-resource "aws_secretsmanager_secret" "llm_ops_secrets" {
-  name = "llm_ops_project_secrets"
+provider "yaml" {}
+
+data "yaml_decode" "aws_secrets" {
+  input = file("${path.module}/../../config/aws_secrets.yaml")
 }
 
 resource "aws_secretsmanager_secret_version" "llm_ops_secrets_version" {
   secret_id = aws_secretsmanager_secret.llm_ops_secrets.id
   secret_string = jsonencode({
-    "AWS_ACCESS_KEY" : var.aws_access_key,
-    "AWS_SECRET_KEY" : var.aws_secret_key,
+    "AWS_ACCESS_KEY" : data.yaml_decode.aws_secrets.content["aws_access_key"],
+    "AWS_SECRET_KEY" : data.yaml_decode.aws_secrets.content["aws_secret_key"],
   })
 }
