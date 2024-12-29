4f56eee40aafa60e2788296ff8535e5e0a564ee7
davestroud <david@davidstroud.me>
2024-12-29 13:56:46 -0500

    add docker build file

--- llm_ops_project/examples/kubeflow_pipeline.py
+++ llm_ops_project/examples/kubeflow_pipeline.py
@@ -1,28 +1,37 @@
+# TODO: Docker for Kubeflow. Where will be the docker image? What will be in the docker image?
+
 from kfp import dsl
 from kfp.components import load_component_from_text
 
+
 @dsl.pipeline(name="LLM Ops Pipeline")
 def llm_ops_pipeline():
-    preprocess_op = load_component_from_text("""
+    preprocess_op = load_component_from_text(
+        """
     name: Preprocess Data
     implementation:
       container:
         image: my-docker-repo/preprocess-image:latest
         command: ["python", "preprocess.py"]
-    """)
-    
-    train_op = load_component_from_text("""
+    """
+    )
+
+    train_op = load_component_from_text(
+        """
     name: Train Model
     implementation:
       container:
         image: my-docker-repo/train-image:latest
         command: ["python", "train.py"]
-    """)
+    """
+    )
 
     preprocess_task = preprocess_op()
-    train_task = train_op(preprocess_task.outputs['processed_data'])
+    train_task = train_op(preprocess_task.outputs["processed_data"])
+
 
 if __name__ == "__main__":
     from kfp.client import Client
+
     client = Client()
     client.create_run_from_pipeline_func(llm_ops_pipeline, arguments={})
